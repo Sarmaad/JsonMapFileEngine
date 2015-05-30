84636970f1f201baacd9e4d4e5e0094077548108
Sarmaad <sarmaad@gmail.com>
2015-05-30 19:33:54 +1000

    added Engine project

--- src/DynamicFileHelperMapping/Program.cs
+++ src/DynamicFileHelperMapping/Program.cs
@@ -4,10 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
-using FileHelpers;
-using FileHelpers.Dynamic;
-using Newtonsoft.Json;
+
+using Engine;
 
 namespace DynamicFileHelperMapping
 {
@@ -15,49 +13,14 @@ namespace DynamicFileHelperMapping
     {
         static void Main(string[] args)
         {
-            var m = JsonConvert.DeserializeObject<Mapping>(File.ReadAllText("mapping.json"));
 
-            var c = new DelimitedClassBuilder(m.Name.Replace(" ", "_"));
-            c.IgnoreEmptyLines = m.IgnoreEmptyLines;
-            c.Delimiter = m.Delimiter;
-            c.IgnoreFirstLines = m.IgnoreFirstLines;
-            c.IgnoreLastLines = m.IgnoreLastLines;
+            var engine = new JsonEngine(File.ReadAllText("mapping.json"));
 
-            var cols = new Dictionary<string,DelimitedFieldBuilder>();
-            foreach (var field in m.Fields.Where(field => !cols.ContainsKey(field.Source)))
-            {
-                cols.Add(field.Source, c.AddField(field.Source, GetType(field.Type)));
-            }
-            
-            foreach (var delimitedFieldBuilder in cols)
-            {
-                delimitedFieldBuilder.Value.FieldQuoted =true;
-                delimitedFieldBuilder.Value.QuoteMode= QuoteMode.OptionalForBoth;
-            }
-            var stype = c.CreateRecordClass();
-            var e = new FileHelperEngine(stype);
-            var d = e.ReadFileAsList("Code-Review-Interview-Data-File.csv");
+            var results = engine.Parse<MemberDetail>("Code-Review-Interview-Data-File.csv");
 
-            var exp = Mapper.CreateMap(stype, typeof (MemberDetail));
-            foreach (var field in m.Fields)
-            {
-                exp.ForMember(field.Destination, opt => opt.MapFrom(field.Source));
-            }
-
-            var mdetails = Mapper.Map<MemberDetail[]>(d);
 
         }
 
-        static Type GetType(string typeName)
-        {
-            switch (typeName)
-            {
-                case "String":
-                    return typeof (string);
-                    break;
-            }
-
-            return null;
-        }
+        
     }
 }
---
+++ src/Engine/JsonEngine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DynamicFileHelperMapping;
+using FileHelpers;
+using FileHelpers.Dynamic;
+using Newtonsoft.Json;
+
+namespace Engine
+{
+    public sealed class JsonEngine
+    {
+        readonly string _jsonMap;
+
+        public JsonEngine(string jsonMap)
+        {
+            _jsonMap = jsonMap;
+        }
+
+        public T[] Parse<T>(string file)
+        {
+            var m = JsonConvert.DeserializeObject<Mapping>(_jsonMap);
+
+            var c = new DelimitedClassBuilder(m.Name.Replace(" ", "_"))
+            {
+                IgnoreEmptyLines = m.IgnoreEmptyLines,
+                Delimiter = m.Delimiter,
+                IgnoreFirstLines = m.IgnoreFirstLines,
+                IgnoreLastLines = m.IgnoreLastLines
+            };
+
+            var cols = new Dictionary<string, DelimitedFieldBuilder>();
+            foreach (var field in m.Fields.Where(field => !cols.ContainsKey(field.Source)))
+            {
+                cols.Add(field.Source, c.AddField(field.Source, GetType(field.Type)));
+            }
+
+            foreach (var delimitedFieldBuilder in cols)
+            {
+                delimitedFieldBuilder.Value.FieldQuoted = true;
+                delimitedFieldBuilder.Value.QuoteMode = QuoteMode.OptionalForBoth;
+            }
+            var stype = c.CreateRecordClass();
+            var e = new FileHelperEngine(stype);
+            var d = e.ReadFileAsList(file);
+
+            var exp = Mapper.CreateMap(stype, typeof(T));
+            foreach (var field in m.Fields)
+            {
+                exp.ForMember(field.Destination, opt => opt.MapFrom(field.Source));
+            }
+
+            return Mapper.Map<T[]>(d);
+        }
+
+        Type GetType(string typeName)
+        {
+            switch (typeName)
+            {
+                case "String":
+                    return typeof(string);
+                    break;
+            }
+
+            return null;
+        }
+    }
+}
